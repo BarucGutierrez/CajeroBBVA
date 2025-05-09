058b1e951c4c64069911b90bde3822781b7b49e9
Baruc Gutierrez <baruc2006gf@gmail.com>
2025-05-09 08:15:15 -0600

    Update cajero chido.c

--- cajero chido.c
+++ cajero chido.c
@@ -87,68 +87,11 @@ scanf("%d", &hrm2);
         default:
             printf("\n\nopcion no valida\n\n");
             break;
-            
+    };    
 
 
         
     
-    int cuenta = login(customer[0], customer[1], customer[2]);    //Llamado de la función encargada de la verificación de la contraseña
-
-    do{
-        printf("*****        Cajero Automatico BBVA        *****\n\n");
-        printf("1.\t Consultar saldo\n");
-        printf("2.\t Depositar dinero\n");
-        printf("3.\t Retirar dinero\n");
-        printf("4.\t Salir\n");
-        printf("\nSeleccione una opcion:\t");
-        scanf("%d", &option);
-        printf("\n************************************************\n\n\n");
-
-        switch(option){
-        case 1: //Muestra el saldo en pantalla
-            printf("*****           Consultar Saldo            *****");
-            printf("\n\nSu saldo es: %.2f\n\n", customer[cuenta].saldo);
-            printf("************************************************\n\n\n");
-            sleep(timer);
-            printf("                     ------\n\n\n");
-            break;
-        case 2: //Manda a llamar a la funcion de cambio y muestra el valor despues de haberlo cambiado
-            printf("*****           Depositar Dinero           *****");
-            hrm1 = customer[cuenta].saldo;
-            customer[cuenta].saldo = cambio(0, hrm1);
-            printf("\nSu saldo actual es: %.2f\n\n", customer[cuenta].saldo);
-            printf("************************************************\n\n\n");
-            sleep(timer);
-            printf("                     ------\n\n\n");
-            break;
-        case 3: //Manda a llamar a la funcion de cambio y muestra el valor despues de haberlo cambiado
-            printf("*****            Retirar Dinero            *****");
-            hrm1 = customer[cuenta].saldo;
-            customer[cuenta].saldo = cambio(1, hrm1);
-            printf("\nSu saldo actual es: %.2f\n\n", customer[cuenta].saldo);
-            printf("************************************************\n\n\n");
-            sleep(timer);
-            printf("                     ------\n\n\n");
-            break;
-        case 4: //Da el mensaje de salida
-            printf("*****                Salida                *****");
-            printf("\n\n\n\tGracias por usar nuestro cajero BBVA\n\n\n");
-            printf("************************************************\n\n\n");
-            sleep(timer);
-            break;
-        default:    //Mesaje si la opcion no existe
-            printf("                     ------\n\n\n");
-            printf("************************************************");
-            printf("\n\tOpcion no valida\n");
-            printf("************************************************\n\n\n");
-            sleep(timer);
-            printf("                     ------\n\n\n");
-            break;
-        }
-
-    }while(option != 4); //Do-While para repetir el proceso hasta que se ingrese una opcion valida
-
-
-
+ 
     return 0;
 }
