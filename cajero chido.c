1b029fdee18337505ab932392dae9fe3d7b844a9
kfloresj <karla.flores7844@alumnos.udg.mx>
2025-05-09 09:55:40 -0600

    Update cajero chido.c

--- cajero chido.c
+++ cajero chido.c
@@ -1,21 +1,24 @@
 #include <stdlib.h>
 #include "ATM.h"
 
+#include <stdlib.h>
+#include "ATM.h"
+
 int main(){
 
-    int option = 0, timer = 2, i=0, hrm2=0, z=0, opcion2=0;
+    int option = 0, timer = 2, i=0, hrm2=0, z=0, option1=0,opcion2=0;
     float hrm1;
     int cuenta;
 
     struct cliente customer[3] = {
-        {1, 1000.00, "Mario Alejandro", 1234, Mexico},
-        {2, 2000.00, "Karla Flores", 5678, Mexico},
-        {3, 3000.00, "Baruc Gutierrez", 9012, Mexico}};
+        {1, 1000.00, "Mario Alejandro", 1234, "Mexico"},
+        {2, 2000.00, "Karla Flores", 5678, "Mexico"},
+        {3, 3000.00, "Baruc Gutierrez", 9012, "Mexico"}};
 
-    printf("Seleccione la Versión del programa:\n");
-    printf("1.\t Usuario\n");
-    printf("2.\t Administrador\n");
-    scanf("%d", &hrm2);
+printf("Seleccione la Versión del programa:\n");
+printf("1.\t Usuario\n");
+printf("2.\t Administrador\n");
+scanf("%d", &hrm2);
 
     switch(hrm2){
         case 1:
@@ -44,7 +47,7 @@ int main(){
                         printf("*****           Depositar Dinero           *****");
                         hrm1 = customer[cuenta].saldo;
                         customer[cuenta].saldo = cambio(0, hrm1);
-                        customer[cuenta].historial[z] = customer[cuenta].saldo - hrm1;  
+                        customer[cuenta].historial[z] = customer[cuenta].saldo - hrm1;
                         z++;
                         printf("\nSu saldo actual es: %.2f\n\n", customer[cuenta].saldo);
                         printf("************************************************\n\n\n");
@@ -91,42 +94,7 @@ int main(){
 
                     switch(option1){
                         case 1:
-
-                            break;
-                        
-                        case 2:
-                            printf("Ingrese el numero del usuario del que quiere buscar su historial:");
-                            scanf("%d", cuenta);
-                            check (customer[cuenta].historial);
-                            break;
-
-                        case 3:
-
-                            break;
-                        
-                        default:
-                            
-                            break;
-                            
-            break;
-
-        default:
-            printf("\n\nopcion no valida\n\n");
-            break;
-
-                        
-    };
-
-
-//Funcion para bsucar usuarios por nombres empieza aquí
-                        //Parte afuera 
-                        char nombres [3][40];
-for (int i = 0; i < 3; i++) {
-        strcpy(nombres[i], customer[i].nombre);
-    }
- char IngresaUsuario[40];
- Bool flag =false;
-            //Parte de adentro del switchcase
+                             //Parte de adentro del switchcase
               printf("1.\t Buscar Usuario por nombre\n");
                     
                         fgets()(IngresaUsuario, sizeof(IngresaUsuario), stdin);
@@ -141,6 +109,33 @@ for (int i = 0; i < 3; i++) {
                     if(flag=false){
                             printf("Error, %s no es un usuario existente en el sistema\n",IngresaUsuario );} 
             //Fin de la funcion para buscar por nombres
+
+
+                            break;
+
+                        case 2:
+                            printf("Ingrese el numero del usuario del que quiere buscar su historial:");
+                            scanf("%d", &cuenta);
+                            check (customer[cuenta].historial);
+                            break;
+
+                        case 3:
+
+                            break;
+
+                        default:
+
+                            break;
+                    }
+            break;
+
+        default:
+            printf("\n\nopcion no valida\n\n");
+            break;
+    };
+
+
+
 
 
     return 0;
